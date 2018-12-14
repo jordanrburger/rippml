ffb5626d9f94937a0e9945ee26c591fdbd44b7e1
Jordan Burger <jordanburger@Jordans-MacBook-Pro.local>
2018-12-14 10:47:26 -0500

    Test

--- cam.py
+++ cam.py
@@ -5,7 +5,7 @@ cap = cv2.VideoCapture(0)
  
 while(True):
     ret, frame = cap.read()
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
  
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
